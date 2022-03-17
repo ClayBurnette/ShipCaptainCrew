0b0625d054789509e70230882850fb67dd86d9df
Clay Burnette <clayburnette13@gmail.com>
2022-03-17 11:01:12 -0400

    Final

---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipCaptainCrew
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Begin Game...");
+            RollGame game = new RollGame();
+            // Introduce The Game To The User
+            string intro =@" 
+            The Game Rules
+            Players Will Take THREE Turns Rolling
+            A Collection Of Five Dice, They Hope To Match Numbers 4, 5, AND 6.
+            The Player Has EXACTLY THREE Rolls To Obtain A 4, 5, AND 6 
+            If The Player Obtains All Three Numbers In Three Rolls
+            That Player Can Start To Accumulate Points
+            The Player Cannot Accumulate Points Without Acquiring The Three Numbers 4, 5, 6, First.
+            The Game Can End In A Draw
+            There Is One Round Per Player";
+            Console.WriteLine(intro);
+
+            bool playAgain = true;
+            while (playAgain)
+            {
+                for (int player = 1; player <= game.numberOfPlayers; player++)
+                {
+                    // Display Whos Turn
+                    DisplayWhosTurnItIs(player);
+                    while (game.numberOfTurns > 0)
+                    {
+                        // Display Remaining Roles
+                        DisplayRemainingTurns(game.numberOfTurns);
+                        // Press Enter To Roll
+                        Console.ReadKey();
+                        List<int> rolled = game.RollDice(game.numberOfDice);
+                        // Display What We Rolled
+                        DisplayDiceRolled(rolled);
+                        // Gather Dice Data From Roll
+                        for (int di = 0; di < rolled.Count - 1; di++)
+                        {
+                            // Remove One Die Per Match If Matched
+                            if (game.runTimeManager.ContainsKey(rolled[di]))
+                            {
+                                if (game.runTimeManager[rolled[di]] == 0)
+                                {
+                                    game.numberOfDice -= 1;
+                                    game.UpdateCard(rolled[di]);
+                                    rolled.RemoveAt(di);
+                                }
+                            }
+                        }
+                        // If Player Gets 4, 5, 6
+                        Console.WriteLine(game.ReturnCard());
+                        int sum = 0;
+                        foreach (int key in game.runTimeManager.Keys)
+                        {
+                            sum += game.runTimeManager[key];
+                        }
+                        if (sum == 3)
+                        {
+                            // The Player Is Currently Scoring
+                            int score = 0;
+                            foreach (int di in rolled)
+                            {
+                                // Add Up Remaining Dice To Score For Player
+                                score += di;
+                            }
+                            // Update The Players Score
+                            game.UpdateScore(player, score);
+                            // Tell The Player How Many Points They Scored
+                            Console.WriteLine("Plus: " + score.ToString() + " Points!!!");
+                        }
+                        // Determine The Points Aquired For Each Roll
+                        game.numberOfTurns -= 1;
+                    } // While (game.numberOfTurns > 0)
+                    DisplayPlayersCurrentsScores(game.ReturnScore());
+                    // Reset Next Player
+                    game.clearStats();
+                }
+                // Lets See The Debugging
+                // Gme Over, Print Scores
+                DisplayPlayersCurrentsScores(game.ReturnScore());
+                // Use Final Scores To Determine Who WON
+                if (game.playerOneScore > game.playerTwoScore)
+                {
+                    Console.WriteLine("Player One Wins!");
+                }
+                else if (game.playerTwoScore > game.playerOneScore)
+                {
+                    Console.WriteLine("Player Two Wins!");
+                }
+                else
+                {
+                    Console.WriteLine("The Game Ends In A Draw!");
+                }
+                // Determine If They Want To Play Again
+                Console.WriteLine("Would You Like To Play Again?");
+                Console.WriteLine("Type 'Y' For Yes, Hit 'Enter' For No.");
+                Console.Write(":");
+                string userInput = Console.ReadLine();
+                // Validatating Input
+                if (!string.Equals(userInput.Trim().ToUpper(), "Y"))
+                {
+                    playAgain = false;
+                }
+            }
+            Console.WriteLine("Game Over!");
+            Console.WriteLine("Press Enter To Exit Game.");
+            Console.ReadKey();
+        }
+        static void DisplayWhosTurnItIs(int player)
+        {
+            Console.WriteLine("It Is Player " + player.ToString() + "'s Turn;");
+        }
+        static void DisplayRemainingTurns(int remainingRolls)
+        {
+            Console.WriteLine("You Have " + remainingRolls.ToString() + " Remaining Rolls.");
+            Console.WriteLine("Press Enter To 'Roll' Your Dice...");
+        }
+        static void DisplayDiceRolled(List<int> diceRolled)
+        {
+            Console.WriteLine("You Rolled:");
+            string print_dice = string.Empty;
+            // Iterate Device
+            // Print Message To Console
+            foreach (int di in diceRolled)
+            {
+                if (diceRolled.IndexOf(di) == diceRolled.Count - 1)
+                {
+                    print_dice += di.ToString();
+                }
+                else
+                {
+                    print_dice += di.ToString() + " ,  ";
+                }
+            }
+            Console.WriteLine(print_dice);
+        }
+        static void DisplayPlayersCurrentsScores(string Score)
+        {
+            Console.WriteLine("Current Scoreboard:");
+            Console.WriteLine(Score);
+        }
+    }
+}
---
+++ RollGame.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipCaptainCrew
+{
+    public class RollGame
+    {
+        /// NOTES:::
+        /// Five Dice
+        /// Each Player has Three Rolls
+        /// Minimum of Two Players
+        /// Roll all dice at once
+        /// 6,5,4 - all at once
+        /// 6 or 5 or 4 - remove that one
+        /// First to order
+        /// English: Human lang def of the game
+        /// The Game 
+        /// Any number of players will take three turns rolling a collection of five dice hoping to match numbers 4, 5, and 6.
+        /// The player has exactly THREE rolls to obtain a 4, 5, and 6 - and if that player obtains all three numbers within
+        /// three rolls - that player can now start accumulating points.
+        /// The player cannot accumulate points without acquiring the three numbers 4, 5, 6, first.
+        /// The game can end in a draw
+        /// There is only one round for each player.
+        /// THIS GAME WILL MOST DEFINITELY TERMINATE AT SOME POINT IN TIME
+        // Note: Game can be played with many players, but we are limiting to two.
+        /// Computer language def of the game
+        /// The game implying that that the memory of the players is keeping track of all previously seen numbers
+        // Number of players in game
+        public int numberOfPlayers { get => 2; set { } }
+        // Number Of Turns
+        public int numberOfTurns { get; set; }
+        // Number Of Starting Dice
+        public int numberOfDice { get; set; }
+        // Each Player Must Be Able To Save Their Score
+        public int playerOneScore { get; set; }
+        public int playerTwoScore { get; set; }
+        // Create A Var To Store Parts Of The Game
+        // This Var Helps Us Know If The Player Landed On One Of These Numbers
+        public int[] rollDice = { 4, 5, 6 };
+        // Create A Var To Store All Roll Results
+        // *Mutable Array*
+        public Dictionary<int, int> runTimeManager;
+        public RollGame()
+        {
+            // Intial Game Settings
+            runTimeManager = new Dictionary<int, int>
+            {
+                { 4, 0 },
+                { 5, 0 },
+                { 6, 0 }
+            };
+            playerOneScore = 0;
+            playerTwoScore = 0;
+            numberOfDice = 5;
+            numberOfTurns = 3;
+        }
+        // Add Data To Runtime Manager
+        public List<int> RollDice(int numberOfDice)
+        {
+            // Roll Given Number Of Dice For Each Roll
+            // Return Array Of Numbers Turned Up
+            List<int> diceRolled = new List<int>();
+            Random roll = new Random();
+            for (int i = 0; i < numberOfDice; i++)
+            {
+                diceRolled.Add(roll.Next(1, 7));
+            }
+            return diceRolled;
+        }
+        // Whos Turn Is It?
+        public int WhosTurnIsIt(int player)
+        {
+            switch (player)
+            {
+                case 1:
+                    return 2;
+                default:
+                    return 1;
+            }
+        }
+        // Return Players Score Card/Data
+        public string ReturnCard()
+        {
+            string message = @"Number of hits:
+            4: {0}
+            5: {1}
+            6: {2}
+            ";
+            return string.Format(message, runTimeManager[4], runTimeManager[5], runTimeManager[6]);
+        }
+        // Method To Update Card At Runtime
+        public void UpdateCard(int diceRolled)
+        {
+            switch (diceRolled)
+            {
+                case 4:
+                    runTimeManager[4] = 1;
+                    break;
+                case 5:
+                    runTimeManager[5] = 1;
+                    break;
+                case 6:
+                    runTimeManager[6] = 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+        // Method To Reduce Number Of Dice
+        public void RemoveDice(int numberToRemove)
+        {
+            numberOfDice = numberOfDice - numberToRemove;
+        }
+        public void clearStats()
+        {
+            runTimeManager[4] = 0;
+            runTimeManager[5] = 0;
+            runTimeManager[6] = 0;
+            numberOfTurns = 3;
+            numberOfDice = 5;
+        }
+        public void UpdateScore(int player, int points)
+        {
+            switch (player)
+            {
+                case 1:
+                    playerOneScore += points;
+                    break;
+                default:
+                    playerTwoScore += points;
+                    break;
+            }
+        }
+        public string ReturnScore()
+        {
+            string message = @"
+            Player 1: {0}
+            Player 2: {1}
+            ";
+            return string.Format(message, playerOneScore, playerTwoScore);
+        }
+    }
+}
